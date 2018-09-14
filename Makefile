e476d364e54f02be12017d6f44db3b56c505c07a
Tyler Bobik <shed412@gmail.com>
2018-09-13 22:20:01 -0400

    added lab1 file

--- Makefile
+++ Makefile
@@ -6,3 +6,6 @@ $(OBJDIR)/%.o: %.c
 
 maintest: $(OBJDIR)/main_test.o
 	gcc -g -o main_test $(LIBDIR)/matrix_lib.c main_test.c
+
+lab1: $(OBJDIR)/lab1.o
+	gcc -g -o lab1 $(LIBDIR)/matrix_lib.c lab1.c
---
+++ lab1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "headers/matrix_lib.h"
+
+int main(int argc, const char* argv[]) {
+
+    vec4 v1 = {1, 2, 3, 4};
+    vec4 v2 = {5, 6, 7, 8};
+
+    /* each row is actually a column */
+    mat4 m1 = {1, -5,  9,   13,
+               2,  6, -10,  14,
+               3,  7,  11,  15,
+               4,  8,  12, -16};
+                
+
+    return 0;
+}
