47d2be63c8ba5b4be60ca61a468034c1a8d02813
Tyler Bobik <shed412@gmail.com>
2018-09-30 22:45:42 -0400

    making the circle from circle.c

--- lab3.c
+++ lab3.c
@@ -9,6 +9,135 @@
 #include "headers/initShader.h"
 #include "headers/matrix_lib.h"
 
+#define NUM_VERTICES 108
 
-int main(int argc, char **argv) {
+vec4* genRandomTriangleColors(int num_vertices);
+vec4* bottom(int num_vertices);
+void init(void);
+void display(void);
+void keyboard(unsigned char key, int mousex, int mousey);
+
+int main(int argc, char **argv) 
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+    glutInitWindowSize(512, 512);
+    glutInitWindowPosition(100,100);
+    glutCreateWindow("Cone");
+    glewInit();
+    init();
+    glutDisplayFunc(display);
+    glutKeyboardFunc(keyboard);
+    glutMainLoop();
+
+    return 0;
+}
+
+/**
+ * From the circle.c file with a couple small changes
+*/
+vec4* genRandomTriangleColors(int num_vertices)
+{
+    GLfloat r, g, b;
+    int index = 0, i;
+
+    srand(time(0));
+
+    vec4 *colors = (vec4 *) malloc(sizeof(vec4) * num_vertices);
+
+    for(i = 0; i < num_vertices / 3; i++)
+    {
+        r = rand() / (float) RAND_MAX;
+        g = rand() / (float) RAND_MAX;
+        b = rand() / (float) RAND_MAX;
+
+        colors[index] = (vec4){r, g, b, 1.0};
+        colors[index + 1] = (vec4){r, g, b, 1.0};
+        colors[index + 2] = (vec4){r, g, b, 1.0};
+        index += 3;
+    }
+
+    return colors;
+}
+
+/**
+ * From the circle.c file with a couple small changes
+*/
+vec4* bottom(int num_vertices)
+{
+    float theta, theta_r, theta10_r;
+    int index = 0;
+    vec4 *vertices = (vec4 *) malloc (sizeof(vec4) * num_vertices);
+    for(theta = 0; theta <= 350; theta += 10)
+    {
+        theta_r = theta * M_PI / 180.0;
+        theta10_r = (theta + 10) * M_PI / 180.0;
+
+        vertices[index] = (vec4){0.0, 0.0, 0.0, 1.0};
+        vertices[index + 1] = (vec4){cos(theta_r), sin(theta_r), 0.0, 1.0};
+        vertices[index + 2] = (vec4){cos(theta10_r), sin(theta10_r), 0.0, 1.0};
+	    index += 3;
+    }
+
+    return vertices;
+}
+
+/**
+ * From the circle.c file with a couple small changes
+*/
+void init(void)
+{
+    GLuint program = initShader("shaders/vshader.glsl", "shaders/fshader.glsl");
+    glUseProgram(program);
+
+    vec4 *circle_vertices = bottom(NUM_VERTICES);
+    vec4 *circle_colors = genRandomTriangleColors(NUM_VERTICES);
+    
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    GLuint buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vec4) * 2 * NUM_VERTICES, NULL, GL_STATIC_DRAW);
+    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vec4) * NUM_VERTICES, circle_vertices);
+    glBufferSubData(GL_ARRAY_BUFFER, sizeof(vec4) * NUM_VERTICES, sizeof(vec4) * NUM_VERTICES, circle_colors);
+
+    GLuint vPosition = glGetAttribLocation(program, "vPosition");
+    glEnableVertexAttribArray(vPosition);
+    glVertexAttribPointer(vPosition, 4, GL_FLOAT, GL_FALSE, 0, (GLvoid *) 0);
+
+    GLuint vColor = glGetAttribLocation(program, "vColor");
+    glEnableVertexAttribArray(vColor);
+    glVertexAttribPointer(vColor, 4, GL_FLOAT, GL_FALSE, 0, (GLvoid *) (sizeof(vec4) * NUM_VERTICES));
+
+    glEnable(GL_DEPTH_TEST);
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glDepthRange(1,0);
+}
+
+/**
+ * From the circle.c file with a couple small changes
+*/
+void display(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glPolygonMode(GL_FRONT, GL_FILL);
+    glPolygonMode(GL_BACK, GL_LINE);
+    glDrawArrays(GL_TRIANGLES, 0, NUM_VERTICES);
+
+    glutSwapBuffers();
+}
+
+/**
+ * From the circle.c file
+*/
+void keyboard(unsigned char key, int mousex, int mousey)
+{
+    if(key == 'q')
+    	exit(0);
+
+    glutPostRedisplay();
 }
