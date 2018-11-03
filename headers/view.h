ed47ae1c3375d2f8454ace54e3d514ce5295999f
Tyler Bobik <shed412@gmail.com>
2018-11-03 02:01:15 -0400

    added normalizze function

---
+++ headers/view.h
@@ -0,0 +1,6 @@
+#ifndef VIEW
+#define VIEW
+
+mat4* look_at(const vec4* eye_pos, const vec4* look_at_pos, const vec4* up_vector);
+
+#endif
---
+++ lib/view.c
@@ -0,0 +1,8 @@
+#include "../headers/view.h"
+#include "../headers/matrix_lib.h"
+
+mat4* look_at(const vec4* eye_pos, const vec4* look_at_pos, const vec4* up_vector) {
+    mat4* ret_mat = calloc(1, sizeof(mat4));
+
+    return ret_mat;
+}
