63ed8bb7c43cbe4e26757807f6a30cc36952275f
Tyler Bobik <shed412@gmail.com>
2018-11-08 19:40:24 -0500

    got rid of color vertices

--- project2.c
+++ project2.c
@@ -28,9 +28,8 @@ mat4 ctm =             {1, 0, 0, 0,
                         0, 0, 1, 0,
                         0, 0, 0, 1};
 
-//vec2 tex_coords[6] = {{0.0, 1.0}, {0.0, 1.0}, {1.0, 0.0}, {0.0, 0.0}, {1.0, 0.0}, {0.0, 0.0}};
+int total_world_vertices = 36;
 
-vec4* genRandomTriangleColors(int num_vertices);
 vec4* bottom(int num_vertices, GLfloat twist, int axis);
 void init(void);
 void display(void);
@@ -54,33 +53,6 @@ int main(int argc, char **argv)
     return 0;
 }
 
-/**
- * From the circle.c file with a couple small changes
-*/
-vec4* genRandomTriangleColors(int num_vertices)
-{
-    GLfloat r, g, b;
-    int index = 0, i;
-
-    srand(time(0));
-
-    vec4 *colors = (vec4 *) malloc(sizeof(vec4) * num_vertices);
-
-    for(i = 0; i < num_vertices / 3; i++)
-    {
-        r = rand() / (float) RAND_MAX;
-        g = rand() / (float) RAND_MAX;
-        b = rand() / (float) RAND_MAX;
-
-        colors[index] = (vec4){r, g, b, 1.0};
-        colors[index + 1] = (vec4){r, g, b, 1.0};
-        colors[index + 2] = (vec4){r, g, b, 1.0};
-        index += 3;
-    }
-
-    return colors;
-}
-
 vec4* ground() {
     vec4* starting_cube = single_cube();
     starting_cube = scale_vertices(starting_cube, VERTS_IN_CUBE, 0.8, 0.1, 0.8);
@@ -92,11 +64,11 @@ vec4* ground() {
 
 pos_tex* ground_with_tex() {
     pos_tex* starting_cube = single_cube_texture(GRASS);
-    vec4* starting_cube_pos = get_pos_verts(starting_cube, 36);
+    vec4* starting_cube_pos = get_pos_verts(starting_cube, VERTS_IN_CUBE);
     starting_cube_pos = scale_vertices(starting_cube_pos, VERTS_IN_CUBE, 0.8, 0.1, 0.8);
     starting_cube_pos = scale_vertices(starting_cube_pos, VERTS_IN_CUBE, 20, 20, 20);
 
-    for (int i = 0; i < 36; i++) {
+    for (int i = 0; i < VERTS_IN_CUBE; i++) {
         starting_cube[i].pos_vert = starting_cube_pos[i];
     }
 
@@ -106,11 +78,11 @@ pos_tex* ground_with_tex() {
 pos_tex* wall_with_tex() {
     int scale_amt = 3;
     pos_tex* starting_cube = single_cube_texture(BRICK);
-    vec4* starting_cube_pos = get_pos_verts(starting_cube, 36);
+    vec4* starting_cube_pos = get_pos_verts(starting_cube, VERTS_IN_CUBE);
     starting_cube_pos = scale_vertices(starting_cube_pos, VERTS_IN_CUBE, 0.1, 0.8, 0.8);
     starting_cube_pos = scale_vertices(starting_cube_pos, VERTS_IN_CUBE, scale_amt, scale_amt, scale_amt);
 
-    for (int i = 0; i < 36; i++) {
+    for (int i = 0; i < VERTS_IN_CUBE; i++) {
         starting_cube[i].pos_vert = starting_cube_pos[i];
     }
 
@@ -120,11 +92,11 @@ pos_tex* wall_with_tex() {
 pos_tex* pillar_with_tex() {
     int scale_amt = 3;
     pos_tex* starting_cube = single_cube_texture(PILLAR);
-    vec4* starting_cube_pos = get_pos_verts(starting_cube, 36);
+    vec4* starting_cube_pos = get_pos_verts(starting_cube, VERTS_IN_CUBE);
     starting_cube_pos = scale_vertices(starting_cube_pos, VERTS_IN_CUBE, 0.15, 0.8, 0.15);
     starting_cube_pos = scale_vertices(starting_cube_pos, VERTS_IN_CUBE, scale_amt, scale_amt, scale_amt);
 
-    for (int i = 0; i < 36; i++) {
+    for (int i = 0; i < VERTS_IN_CUBE; i++) {
         starting_cube[i].pos_vert = starting_cube_pos[i];
     }
 
@@ -142,9 +114,8 @@ void init(void)
     glUseProgram(program);
 
     pos_tex* ground_tex_pos = pillar_with_tex();
-    vec4 *circle_colors = genRandomTriangleColors(NUM_VERTICES);
-    vec2* tex_coords = get_tex_verts(ground_tex_pos, 36);
-    vec4 *ground_vertices = get_pos_verts(ground_tex_pos, 36);
+    vec2* tex_coords = get_tex_verts(ground_tex_pos, VERTS_IN_CUBE);
+    vec4 *ground_vertices = get_pos_verts(ground_tex_pos, VERTS_IN_CUBE);
 
     vec4 eyes = {0.0, 1.5, -3, 1};
     vec4 look_at_pos = {0, 1.2, 0, 1};
@@ -174,12 +145,10 @@ void init(void)
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
     glBufferData(GL_ARRAY_BUFFER
-        , sizeof(vec4) * 36 + sizeof(vec4)*36 + sizeof(vec2) *36 
+        , sizeof(vec4) * total_world_vertices + sizeof(vec4)*total_world_vertices + sizeof(vec2) *total_world_vertices 
         , NULL, GL_STATIC_DRAW);
-    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vec4) * 36, ground_vertices);
-    glBufferSubData(GL_ARRAY_BUFFER, sizeof(vec4) * 36, sizeof(vec4) * 36, circle_colors);
-    glBufferSubData(GL_ARRAY_BUFFER, sizeof(vec4) * 36 + sizeof(vec4) * 36
-        , sizeof(vec2) *36, tex_coords);
+    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vec4) * total_world_vertices, ground_vertices);
+    glBufferSubData(GL_ARRAY_BUFFER, sizeof(vec4) * total_world_vertices , sizeof(vec2) *total_world_vertices, tex_coords);
 
 
     GLuint vPosition = glGetAttribLocation(program, "vPosition");
@@ -188,11 +157,11 @@ void init(void)
 
     GLuint vColor = glGetAttribLocation(program, "vColor");
     glEnableVertexAttribArray(vColor);
-    glVertexAttribPointer(vColor, 4, GL_FLOAT, GL_FALSE, 0, (GLvoid *) (sizeof(vec4) * NUM_VERTICES));
+    glVertexAttribPointer(vColor, 4, GL_FLOAT, GL_FALSE, 0, (GLvoid *) (sizeof(vec4) * total_world_vertices));
 
     GLuint vTexCoord = glGetAttribLocation(program, "vTexCoord");
     glEnableVertexAttribArray(vTexCoord);
-    glVertexAttribPointer(vTexCoord, 2, GL_FLOAT, GL_FALSE, 0, (GLvoid *) 0 + (sizeof(vec4) * NUM_VERTICES) + (sizeof(vec4) * NUM_VERTICES));
+    glVertexAttribPointer(vTexCoord, 2, GL_FLOAT, GL_FALSE, 0, (GLvoid *) 0 + (sizeof(vec4) * total_world_vertices));
 
     GLuint texture_location = glGetUniformLocation(program, "texture");
     glUniform1i(texture_location, 0);
@@ -221,7 +190,7 @@ void display(void)
     glUniformMatrix4fv(perspective_shift, 1, GL_FALSE, (GLfloat *) &projection);
     glUniformMatrix4fv(cam_shit, 1, GL_FALSE, (GLfloat *) &model_view);
 
-    glDrawArrays(GL_TRIANGLES, 0, NUM_VERTICES);
+    glDrawArrays(GL_TRIANGLES, 0, total_world_vertices);
 
     glutSwapBuffers();
 }
